4c0796ea7cefda7cd38b9b8e53aea1dccbed13c2
Nguyen Tran Tuan Khai <tuankhai140206@gmail.com>
2025-01-02 21:01:49 +0700

    initialize post-view branch

---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,23 @@
+// import modules
+import React, { useEffect, useState } from "react";
+
+// import assets
+import HashIcon from '../assets/hash.png';
+import SaveIcon from '../assets/save.svg';
+import ShareIcon from '../assets/share.svg';
+import TerminalIcon from '../assets/terminal.svg';
+import VoteIcon from '../assets/vote.svg';
+
+// import style
+import '../styles/Post.css';
+
+export default function Post({ isComment }){
+    return (
+        <>
+            <div className="post-window">
+                
+            </div>
+        </>
+    );
+
+}
