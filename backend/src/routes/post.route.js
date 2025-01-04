412073601486ffb52da1d9bbfeb56d50d7683205
Phan Hạo Lương <phanhaoluong@gmail.com>
2025-01-04 15:44:49 +0700

    comment route

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import post from "../models/post.model.js";
 import user from '../models/user.model.js';
+import Comment from '../models/comment.model.js';
 
 const router = express.Router();
 
@@ -31,4 +32,43 @@ router.delete('/delete/:postid', async (req, res) => {
     }
 })
 
+//to get a single post, WIP
+router.get('/:postid', async (req, res) => {
+    try {
+        const postid = req.params.postid
+        const singlepost = await post.findById(postid)
+        .populate('author', 'name')
+        .populate({path:'comments', populate:{path:'author', select:'name'},select:'content code'})
+    
+        res.status(200).json(singlepost);
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+//to comment on a post, WIP
+router.post('/:postid/comment', async (req, res) => {
+    const postid = req.params.postid
+    const newComment = new Comment(Object.assign(req.body, {parent_post: postid}))
+    try {
+        await post.updateOne({_id: postid}, {$push: {comments: newComment._id}})
+        await newComment.save();
+        res.status(200).send("OK")
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+//to delete a comment, WIP
+router.delete('/:postid/deletecomment', async (req, res) => {
+    const postid = req.params.postid
+    const commentid = req.body.commentid
+    try {
+        await post.updateOne({_id: postid}, {$pull: {comments: commentid}}) 
+        await Comment.deleteOne({_id: commentid})
+        res.status(200).send("OK")
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
 export default router
