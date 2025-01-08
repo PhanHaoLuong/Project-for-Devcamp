4e782c3524026cf43e0aef83b45d5cdbaea6252d
Phan Hạo Lương <phanhaoluong@gmail.com>
2025-01-08 21:29:53 +0700

    protected-route WIP 8/1

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import fileUpload from "express-fileupload";
+import cookieParser from "cookie-parser"
 
 import { connectDB } from "./config/db.js";
 import { get_forum_posts } from "./controllers/post.controller.js";
@@ -19,10 +20,11 @@ const PORT = process.env.PORT;
 
 const corsoptions = { origin: "http://localhost:5173" }
 
-
 const app = express()
+
 app.use(express.json())
 app.use(cors(corsoptions))
+app.use(cookieParser())
 
 app.listen(PORT, () => {
     connectDB(); 
@@ -38,7 +40,6 @@ app.get('/forum', get_forum_posts)
 app.post('/fileupload', fileUpload({createParentPath: true}),async (req, res) => {
     const files = req.files
     console.log(files.file.data)
-    console.log(files.file.data.toString())
     const save = {'author': '6778ad96d751b1b21cfb06cc', 'data': files.file.data}
     
     const newFile = new file(save)
