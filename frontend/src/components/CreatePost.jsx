bf3721906dedf8bce7f0423525415734d1a5545e
Nam Khanh <namkhanh02102006@gmail.com>
2025-01-10 03:14:32 +0700

    Update CreatePost.jsx

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -6,37 +6,27 @@ export default function CreatePost() {
   return (
     <div className="main-container">
       <div className="frame">
-        <div className="flex-row-f">
+        <div className="upper-bar1">
           <div className="icon-create-post">
             <div className="post-icon" />
             <span className="create-post-text">create post</span>
           </div>
-          <div className="upper-bar" />
         </div>
         <span className="title">Title</span>
         <input className="title-space" />
-
         <span className="tags-text">Tags</span>
         <button className="tag-adder-button">
-        <div className="box-2">
-            <span className="plus">+Tag</span>
-          </div>
-          <div className="tag-adder" />
+           add tags
         </button>
-
         <span className="description-text">Description</span>
-        <input className="description-space" />
-        <div className="flex-row-e">
+        <textarea className="description-space" />
           <button className="write-code-button">
-            <span className="write-code-text">Write code</span>
+            Write code
           </button>
-        
           <button className="add-files-button">
-              <span className="add-files">Add files</span>
-           
+            Add files
           </button>
         </div>
       </div>
-    </div>
   );
 }
