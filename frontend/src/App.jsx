7b15a0e8f9d8cd46c4803b8440756f8e1e02a078
Phan Hạo Lương <phanhaoluong@gmail.com>
2025-01-09 22:57:13 +0700

    Navbar authUser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,20 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query'
 
-// import pages
-import UserAuth from './pages/UserAuth';
-import UserSignUp from './pages/UserSignUp'
-
 import * as pageAddress from './pages/page-address.json'
 
 /* import components */
 import Navbar from "./components/Navbar";
+import MiniPost from "./components/MiniPost";
+import Fileupload from "./pages/File upload test";
+import FileItem from "./components/FileItem";
 
-import Vote from './components/Vote';
 
 /* import pages */
+import FullPostPage from "./pages/FullPostPage";
 import View from "./pages/View";
-import Home from "./pages/Home"; 
 import UserSignUp from './pages/UserSignUp';
+import UserAuth from './pages/UserAuth';
 import Home from "./pages/Home";
 import User from "./pages/User";
 import Saved from "./pages/Saved";
@@ -37,20 +36,25 @@ function App() {
           },
           credentials: 'include'
         })
-        console.log("response: ", response)
-        return response.json()
+        if (response.status === 200) {
+          return response.json()
+        }
+        else {
+          return null
+        }
       } catch (error) {
         return { error }
       }
     }
   })
-
+  
+  console.log("authUser: ", authUser)
   if (isLoading) return null;
 
   return (
     <>
       <Router>
-        <Navbar isLoggedIn={isLoggedIn} />
+        <Navbar isLoggedIn={authUser ? true : false} />
         <Routes>
           <Route path={pageAddress.home} element={<><h1 style={{color:'white'}}>homepage placeholder</h1><a href="/auth/login">login</a></>}/>
           <Route path={pageAddress.login} element={!authUser ? <UserAuth /> : <Navigate to="/"/>} />
