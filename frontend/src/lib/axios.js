baf7f9d4bbb6d98d7615605893ed28ab00b7ecc9
Phan Hạo Lương <phanhaoluong@gmail.com>
2025-03-04 21:10:18 +0700

    Revert "Updated the axios baseURL"
    This reverts commit 9c0323831fd44ff5020d91b488646c3ac973987c.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000' : 'https://project-for-devcamp-production.vercel.app',
+    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000' : '',
     withCredentials: true,
 });
