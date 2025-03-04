9c0323831fd44ff5020d91b488646c3ac973987c
Phan Hạo Lương <phanhaoluong@gmail.com>
2025-03-04 20:55:36 +0700

    Updated the axios baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000' : '',
+    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5000' : 'https://project-for-devcamp-production.vercel.app',
     withCredentials: true,
 });
